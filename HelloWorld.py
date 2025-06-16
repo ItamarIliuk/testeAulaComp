25e9b8f86cf826e87937438203aae745deb46a5f
ItamarIliuk <itamar.iliuk@gmail.com>
2025-06-16 11:39:10 -0300

    add new messages

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,4 @@
 print("Hello, World!")
 print("This is a test of the Python environment setup.")
+print("Let's ensure everything is working correctly.")
+print("If you see this message, the code is running successfully.")
